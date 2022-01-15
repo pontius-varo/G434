e1c3c8417f946a6d69d5b7ad325889d72cc881ca
Antonio Ponce <zponce@posteo.net>
2022-01-15 13:34:40 -0500

    1.1 Changes| Check README for details

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ A python script commissioned to create a set of NFTs with metadata
 
 Requires the following libraries: PIL, IPython
 
+
+## 1.1 ChangeLog
+* Created getWeights function to prompt user for weights and store those values in a returned array
+* Created total_images variable where user prompt for nft amount given by user is stored
+
 ## 1.0 ChangeLog
 * Created function getAssets to retrieve an array of objects with certain values based on  the ./Assets directory
 * Created function getAssetKeys to retrieve keys from each object in the getAssets array
--- neoscript.py
+++ neoscript.py
@@ -39,19 +39,61 @@ def getAssetKeys(array):
     return mykeys
 
 
+def getWeights(keys):
+    # Create an empty array to export weights
+    weights = []
+    # Loop through each key array
+    for key_array in keys:
+        # Create another empty array to organize weights
+        key_weights = []
+        try:
+            # For each key in the key array
+            for key in key_array:
+                # Ask the user to give a weight value
+                print(f'Enter weight value for key {key}')
+                key_weight = input("> ")
+                # Add value as an interger
+                key_weights.append(int(key_weight))
+
+            weights.append(key_weights)
+
+        # Catch any errors (especially if a user enters a character) and break
+        except Exception as e:
+            print(f'An error occured: {e}')
+            break
+    return weights
+
+
+# This script returns all the asset arrays from the subdirectories in Alphabetical order
+# Therefore, instead of assigning all values to different variables, we can just retrieve them from the array returned
+
+# in this case where we have the following subdirectories in ./Assets
+# [Accesories, Armor, Background, Hat, Head, LeftArm, RightArm]
+# getAssets returns the following Objects with key value pairs {"shortened-key" : "file.extension"}
+# [{Accesories}, {Armor}, {Background}, {Hat}, {Head}, {LeftArm}, {RightArm}]
+# From those assets we retrieve the keys created using getAssetKeys
+# those keys are then consumed by getWeights, which after prompting the user to assign
+# a weight to each key, returns an array of weight arrays in a similar order to getAssets ~>
+# [[Accessory_weights], [Armor_weights], [Background_weights], [Hat_weights], [Head_weights], [LeftArm_weights], [RightArm_weights]]
+# Therefore instead of creating specific arrays for a fixed number of categories, we can create them dynamically
+# As a result, when it is time to assemble the nft, we can simply interate through first the nft_assets array and then
+# the weights array to establish rarity
+
 # Retrieve Asset objects based on files in ./Assets
 nft_assets = getAssets(path)
-
-# For now, assign them to variables
-accessory_files = nft_assets[0]
-armor_files = nft_assets[1]
-background_files = nft_assets[2]
-hat_files = nft_assets[3]
-head_files = nft_assets[4]
-left_arm_files = nft_assets[5]
-right_arm_files = nft_assets[6]
 
 # Retrieve Asset keys
 asset_keys = getAssetKeys(nft_assets)
 
 # Assign weights to keys, prompt the user and have them give input
+weights = getWeights(asset_keys)
+
+# Prompt the User to retrive the amount of nfts to be generated
+print("Please give the numerical amount of nfts you want created")
+total_images = int(input("> "))
+
+# Assign incompatible traits
+
+# Leave incompatible traits funciton here
+
+# Leave nft image creation here
