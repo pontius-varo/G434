b840cb3e4ce7e27d17616ffeba13b78050402be2
Antonio Ponce <zponce@posteo.net>
2022-01-20 21:59:37 -0500

    Check Changelog 1.2

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A python script commissioned to create a set of NFTs with metadata
 Requires the following libraries: PIL, IPython
 
 
+## 1.2 ChangeLog
+* Created createIncompatibles, catchIncompatibles, newImage functions
+* Added incompatible_active variable to determine whether or not to look for incompatibles
+* Added default categories to newImage function (Will be changed in the future!)
 ## 1.1 ChangeLog
 * Created getWeights function to prompt user for weights and store those values in a returned array
 * Created total_images variable where user prompt for nft amount given by user is stored
--- neoscript.py
+++ neoscript.py
@@ -5,6 +5,25 @@ import os
 import json
 
 
+# Assign incompatible traits
+incompatible_active = False
+
+# This script returns all the asset arrays from the subdirectories in Alphabetical order
+# Therefore, instead of assigning all values to different variables, we can just retrieve them from the array returned
+# in this case where we have the following subdirectories in ./Assets
+# [Accesories, Armor, Background, Hat, Head, LeftArm, RightArm]
+# getAssets returns the following Objects with key value pairs {"shortened-key" : "file.extension"}
+# [{Accesories}, {Armor}, {Background}, {Hat}, {Head}, {LeftArm}, {RightArm}]
+# From those assets we retrieve the keys created using getAssetKeys
+# those keys are then consumed by getWeights, which after prompting the user to assign
+# a weight to each key, returns an array of weight arrays in a similar order to getAssets ~>
+# [[Accessory_weights], [Armor_weights], [Background_weights], [Hat_weights], [Head_weights], [LeftArm_weights], [RightArm_weights]]
+# Therefore instead of creating specific arrays for a fixed number of categories, we can create them dynamically
+# As a result, when it is time to assemble the nft, we can simply interate through first the nft_assets array and then
+# the weights array to establish rarity
+# Retrieve Asset objects based on files in ./Assets
+
+
 path = './Assets/'
 
 
@@ -50,8 +69,9 @@ def getWeights(keys):
             # For each key in the key array
             for key in key_array:
                 # Ask the user to give a weight value
-                print(f'Enter weight value for key {key}')
-                key_weight = input("> ")
+                #print(f'Enter weight value for key {key}')
+                #key_weight = input("> ")
+                key_weight = '10'
                 # Add value as an interger
                 key_weights.append(int(key_weight))
 
@@ -63,23 +83,88 @@ def getWeights(keys):
             break
     return weights
 
+# Should take an object instead of two distinct arrays
+# For example : INCOMPANTIBLE_TRAITS = {"RIGHTARM": ["TRIDENT", "SPEAR"], "LEFTARM": ["TRIDENT", "SPEAR"]}
+# catchTraits should take the key, loop through the subarray and compare them
+#  to the traits being passed through after image generation
 
-# This script returns all the asset arrays from the subdirectories in Alphabetical order
-# Therefore, instead of assigning all values to different variables, we can just retrieve them from the array returned
 
-# in this case where we have the following subdirectories in ./Assets
-# [Accesories, Armor, Background, Hat, Head, LeftArm, RightArm]
-# getAssets returns the following Objects with key value pairs {"shortened-key" : "file.extension"}
-# [{Accesories}, {Armor}, {Background}, {Hat}, {Head}, {LeftArm}, {RightArm}]
-# From those assets we retrieve the keys created using getAssetKeys
-# those keys are then consumed by getWeights, which after prompting the user to assign
-# a weight to each key, returns an array of weight arrays in a similar order to getAssets ~>
-# [[Accessory_weights], [Armor_weights], [Background_weights], [Hat_weights], [Head_weights], [LeftArm_weights], [RightArm_weights]]
-# Therefore instead of creating specific arrays for a fixed number of categories, we can create them dynamically
-# As a result, when it is time to assemble the nft, we can simply interate through first the nft_assets array and then
-# the weights array to establish rarity
+def createIncompatibles():
+    incomp = {}
 
-# Retrieve Asset objects based on files in ./Assets
+    num = int(input(
+        'Please enter the number of incompatible traits you have: '))
+
+    x = 0
+
+    while x < num:
+        trait_key = input("Enter the base trait: ")
+        num_of_incomps = int(input(
+            f'How many incompatibles does trait {trait_key} have?: '))
+
+        count = 0
+
+        incomp_array = []
+
+        # The Data struct for this needs to be changed.
+        # the CATEGORY must be included somehow, as it will be used to FIND
+        # an incompatible trait, which would be better than EXCLUDING an
+        # entire category all together
+        while count < num_of_incomps:
+            incompatible_trait = input(
+                f'Enter your incompatible category for {trait_key}: ')
+
+            incomp_array.append(incompatible_trait)
+            count += 1
+
+        print(
+            f'OK finshed adding {trait_key} with the following incompatible categories its: {incomp_array}')
+        incomp[trait_key] = incomp_array
+        x += 1
+
+    return incomp
+
+
+def catchIncompatibles(incompatible_obj, newimage, active):
+
+    if(active):
+        for trait in incompatible_obj:
+            for category in newimage:
+                if(newimage[category] == trait):
+                    for incompatible in incompatible_obj[trait]:
+                        if(newimage[incompatible] != "None"):
+                            print(f'BEFORE {newimage}')
+                            newimage[incompatible] = "None"
+                            print("Incompatible found! Changing...")
+                            print(f'AFTER {newimage}')
+                        else:
+                            print("Not!")
+
+                else:
+                    print("Pass!")
+
+    return newimage
+
+
+def newImage():
+
+    # CHANGE IF YOU HAVE DIFFERENT CATEGORIES
+    categories = ["Accesories", "Armor", "Background",
+                  "Hat", "Head", "LeftArm", "RightArm"]
+    new_image = {}
+
+    for x in range(0, len(categories)):
+        new_image[categories[x]] = random.choices(asset_keys[x], weights[x])[0]
+
+    cleaned_image = catchIncompatibles(
+        incompatible_traits, incompatible_active)
+
+    if new_image in all_images:
+        return newImage()
+    else:
+        return cleaned_image
+
+
 nft_assets = getAssets(path)
 
 # Retrieve Asset keys
@@ -92,8 +177,20 @@ weights = getWeights(asset_keys)
 print("Please give the numerical amount of nfts you want created")
 total_images = int(input("> "))
 
-# Assign incompatible traits
-
-# Leave incompatible traits funciton here
+if(input("Would you like to assign incompatible traits? (y/n)> ") == 'y'):
+    incompatible_active = True
+    incompatible_traits = createIncompatibles()
+    print(incompatible_traits)
+else:
+    pass
 
 # Leave nft image creation here
+all_images = []
+
+
+# Creates all new Images and adds them to all_images
+for i in range(total_images):
+
+    new_trait_image = newImage()
+
+    all_images.append(new_trait_image)
